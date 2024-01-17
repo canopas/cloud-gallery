8f08314291ae953e50f1e6901b135cb75da6e7ba
Pratik-canopas <pratik.k@canopas.com>
2024-01-17 19:13:10 +0530

    Implement google sign in

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_gallery/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -6,7 +7,9 @@ import 'ui/app.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   final container = ProviderContainer();
   runApp(UncontrolledProviderScope(
