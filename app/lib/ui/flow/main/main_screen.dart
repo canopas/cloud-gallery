94179af4d004fe85b10c8614a182d7f311291b2e
Pratik-canopas <pratik.k@canopas.com>
2024-01-17 19:20:00 +0530

    Remove unused code

--- app/lib/ui/flow/main/main_screen.dart
+++ app/lib/ui/flow/main/main_screen.dart
@@ -1,6 +1,4 @@
-import 'package:cloud_gallery/ui/flow/main/main_screen_view_model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,26 +11,9 @@ class _MainScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-        body: Center(
-          child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [Button()],
-              ),
-        ));
-  }
-}
-
-class Button extends ConsumerWidget {
-  const Button({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final notifier = ref.read(mainScreenViewNotifierProvider.notifier);
-    return TextButton(
-        onPressed: () {
-          notifier.getFiles();
-        },
-        child: const Text("Get DriveFiles"));
+      body: Center(
+        child: Text('Home Screen'),
+      ),
+    );
   }
 }
