3cbf9d371a86dc0bf64abafbc7a97c0ca13d2e58
cp-sneha-s <sneha.s@canopas.com>
2024-04-15 10:55:17 +0530

    Minor change

--- app/ios/Runner/AppDelegate.swift
+++ app/ios/Runner/AppDelegate.swift
@@ -9,6 +9,7 @@ import workmanager
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+     // WorkmanagerPlugin.registerTask(withIdentifier: "cloud-gallery-background-process")
     WorkmanagerPlugin.registerBGProcessingTask(withIdentifier: "cloud-gallery-background-process")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
