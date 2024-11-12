584d543feebef5310ceeb9aaf068fb9edc776045
Pratik-canopas <pratik.k@canopas.com>
2024-11-12 16:07:57 +0530

    Upgrade dependencies

--- data/lib/errors/app_error.dart
+++ data/lib/errors/app_error.dart
@@ -69,9 +69,7 @@ class UnableToSaveFileInGallery extends AppError {
 }
 
 class SomethingWentWrongError extends AppError {
-  const SomethingWentWrongError({String? message, int? statusCode})
+  const SomethingWentWrongError({super.message, super.statusCode})
       : super(
-            l10nCode: AppErrorL10nCodes.somethingWentWrongError,
-            message: message,
-            statusCode: statusCode);
+            l10nCode: AppErrorL10nCodes.somethingWentWrongError);
 }
